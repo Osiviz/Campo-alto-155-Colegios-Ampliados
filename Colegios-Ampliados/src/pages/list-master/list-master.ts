aeb5d843aea977ba70cf7f8bb9b8c5f0209dbf4a
jairoareyes <jairo.314@hotmail.com>
2018-09-27 15:53:53 -0500

    Cambio 3

--- Colegios-Ampliados/src/pages/list-master/list-master.ts
+++ Colegios-Ampliados/src/pages/list-master/list-master.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, ModalController } from 'ionic-angular';
 
 import { Item } from '../../models/item';
 import { Items } from '../../providers';
@@ -10,10 +10,10 @@ import { Items } from '../../providers';
   templateUrl: 'list-master.html'
 })
 export class ListMasterPage {
-  //currentItems: Item[];
+  currentItems: Item[];
 
-  constructor(public navCtrl: NavController, public items: Items) {
-    
+  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController) {
+    this.currentItems = this.items.query();
   }
 
   /**
@@ -23,6 +23,60 @@ export class ListMasterPage {
   }
 
 
+  /**
+   * Perform a service for the proper items.
+   */
+  getItems(ev) {
+    let val = ev.target.value;
+    if (!val || !val.trim()) {
+      this.currentItems = [];
+      return;
+    }
+    this.currentItems = this.items.query({
+      name: val
+    });
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  openItem(item: Item) {
+    this.navCtrl.push('ItemDetailPage', {
+      item: item
+    });
+  }
+
+
+    /**
+   * Delete an item from the list of items.
+   */
+  deleteItem(item) {
+    this.items.delete(item);
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  /*openItem(item: Item) {
+    this.navCtrl.push('ItemDetailPage', {
+      item: item
+    });*/
+
+
+
+      /**
+   * Prompt the user to add a new item. This shows our ItemCreatePage in a
+   * modal and then adds the new item to our data source if the user created one.
+   */
+  addItem() {
+    let addModal = this.modalCtrl.create('ItemCreatePage');
+    addModal.onDidDismiss(item => {
+      if (item) {
+        this.items.add(item);
+      }
+    })
+    addModal.present();
+  }
 
 
   }
